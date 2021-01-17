87b92cbd8e29466d78c7722de9bcf1749f825c25
xiexun <38444425@qq.com>
2021-01-17 21:59:13 +0800

    页面修改

--- pages/order/orderIndex.js
+++ pages/order/orderIndex.js
@@ -88,7 +88,7 @@ Page({
     },
 
     toOrderDetail(e){
-        let id=e.currentTarget.dataset.item.userId
+        let id=e.currentTarget.dataset.item.trainerId
         // wx.navigateTo({
         //     url:'/pages/order/orderDetail/detail?id='+id+'?type='+this.data.activeIndex
         // })
